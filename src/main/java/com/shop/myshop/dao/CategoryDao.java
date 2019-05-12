929f16f63a3b76fb2a36412ade5d0a7365abdf10
Toom <toom1970@163.com>
2019-05-12 18:21:10 +0800

    nothing

--- src/main/java/com/shop/myshop/dao/CategoryDao.java
+++
@@ -1,9 +0,0 @@
-package com.shop.myshop.dao;
-
-import com.shop.myshop.pojo.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryDao extends JpaRepository<Category, Integer> {
-
-    Category findByName(String name);
-}
